29edec7445e22358605dddd3a313988b71a53e72
Vishnu Krishnathu <vishnu.krishnathu1999@gmail.com>
2021-09-08 23:07:37 +0530

    design fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import firebase from "firebase/app";
-import { CookiesProvider } from "react-cookie";
 import Navbar from "../components/Navbar";
 import AuthContext, { AuthFunction } from '../Context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,13 +8,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (<>
-  <CookiesProvider>
     <AuthContext>
       <Navbar>
           <Component {...pageProps} />
       </Navbar>
     </AuthContext>
-  </CookiesProvider>
  </>)
 }
 export default MyApp
